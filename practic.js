af5d45ef7b42b8ca6889d2b3bee5e8ded6f8f0ae
Jewel2544 <jewelranajahid2544@gmail.com>
2026-02-03 16:10:56 +0600

    practic java scripte

---
+++ practic.js
@@ -0,0 +1,25 @@
+// number type
+var glass = 80;
+// sting
+var subject = "jahid"
+// boolean
+var passed = true;
+
+// NAMING CONVENTION
+/**
+ * 1.single word
+ * 2.no quote
+ * 3.no gap or dash 
+ * 4.can not start with number 
+ * 5.van not use any keyword
+ * 6.use camelcase
+ */
+
+var lastExamResult = 68;
+
+
+/**
+ * oparetion
+ *  + - * / %
+ * 
+ */
